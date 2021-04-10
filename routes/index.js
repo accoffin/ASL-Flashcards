0773251e4381c396253efbb4152b01c2ba77b64e
accoffin <76799599+accoffin@users.noreply.github.com>
2021-04-10 14:29:43 -0600

    Fixed index.js express router.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-const router = require("express").Router();
-const express = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -8,7 +8,7 @@ router.get("/", (req, res, next) => {
 
 router.get("/profile", (req, res, next) => {
   console.log(req.query);
-  res.render("profile" , { user: req.session && req.session.user });
+  res.render("profile", { user: req.session && req.session.user });
 });
 
 module.exports = router;
