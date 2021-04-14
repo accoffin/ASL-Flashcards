d60fbbee3d590dd7349dfeb28c94a83ad3c4c73c
accoffin <76799599+accoffin@users.noreply.github.com>
2021-04-13 19:34:16 -0600

    Add secret dance gif.

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get("/profile", (req, res, next) => {
   res.render("profile", { user: req.session && req.session.user });
 });
 
+router.get("/deafAF", (req, res, next) => {
+  console.log(req.query);
+  res.render("secrets/deafAF");
+});
+
 module.exports = router;
