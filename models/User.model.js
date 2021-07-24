d690ec5e3ed1da1782c3778db8ec7f44ae6c0d2f
Derek <shouldergiant@icloud.com>
2021-07-23 23:26:13 -0400

    ASL Flashcards User Model - version 2

--- models/User.model.js
+++ models/User.model.js
@@ -7,9 +7,12 @@ const userSchema = new Schema({
     type: String,
     unique: true,
   },
+  email: {type: String, unique: true},
   passhash: { type: String, required: true},
   isAdmin: { type: Boolean, default: false }, //if 'true' user will have access to flashcard.create page.
   decks: [{ type: Schema.Types.ObjectId, ref: "Deck" }],
+  currentDeck: {type: Schema.Types.ObjectId, ref: "Deck"},
+  currentMode: {type: String, enum: ["expressive", "receptive"]},
 });
 
 const User = model("User", userSchema);
