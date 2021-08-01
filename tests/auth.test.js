72bdeaf6141d1eef21a70e94bc6fb3d2a65beb4d
Derek <shouldergiant@icloud.com>
2021-08-01 00:06:40 -0400

    Added teardown for signup tests

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,56 +1,55 @@
 const request = require("supertest");
 const app = require("../app");
 
+const User = require("../models/User.model");
+
 describe("Test the signup route", () => {
+  afterAll((done) => {
+    User.findOneAndDelete({ username: "TESTABOB" }).exec();
+  });
+
   test("POST /auth/signup responds with user data and session", () => {
     const TEST_USER = "TESTABOB";
     const TEST_PASSWORD = "1two3Four_flyya38480583yfklg";
     const TEST_ADMIN = false;
     return request(app)
       .post("/auth/signup")
-      .send({ username:TEST_USER, password:TEST_PASSWORD, isAdmin:TEST_ADMIN })
-      .then(response => {
+      .send({
+        username: TEST_USER,
+        password: TEST_PASSWORD,
+        isAdmin: TEST_ADMIN,
+      })
+      .then((response) => {
+        console.log(response);
         expect(response.statusCode).toBe(201);
       });
   });
 
-  test("Error for missing username", () => {
-  });
+  test("Error for missing username", () => {});
 
-  test("Error for username already taken", () => {
-  });
+  test("Error for username already taken", () => {});
 
-  test("Error for missing password", () => {
-  });
+  test("Error for missing password", () => {});
 
-  test("Error for invalid password", () => {
-  });
+  test("Error for invalid password", () => {});
 
-  test("Error when header already has authorization", () => {
-  });
+  test("Error when header already has authorization", () => {});
 });
 
 describe("Test the login route", () => {
-  test("POST /auth/login responds with User and Session", () => {
-  });
+  test("POST /auth/login responds with User and Session", () => {});
 
-  test("Error for missing username", () => {
-  });
+  test("Error for missing username", () => {});
 
-  test("Error for unregistered username", () => {
-  });
+  test("Error for unregistered username", () => {});
 
-  test("Error for incorrect password", () => {
-  });
+  test("Error for incorrect password", () => {});
 
-  test("Error when header already has authorization", () => {
-  });
+  test("Error when header already has authorization", () => {});
 });
 
 describe("Test the logout route", () => {
-  test("POST /auth/logout responds with success", () => {
-  });
+  test("POST /auth/logout responds with success", () => {});
 
-  test("Error when header has no authorization", () => {
-  });
+  test("Error when header has no authorization", () => {});
 });
