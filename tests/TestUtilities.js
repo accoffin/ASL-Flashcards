ac5144be73b85e9d4822e83c99b3b4ff5a891101
Derek <shouldergiant@icloud.com>
2021-08-10 17:31:25 -0400

    implemented data teardown utility for testing. Mock User utility implementation in progress

--- tests/TestUtilities.js
+++ tests/TestUtilities.js
@@ -3,11 +3,54 @@ const Session = require("../models/Session.model");
 const Deck = require("../models/Deck.model");
 const Flashcard = require("../models/Flashcard.model");
 
+const MODELS = {
+  SESSION: "session",
+  USER: "user",
+  DECK: "deck",
+  CARD: "card",
+};
+
 module.exports = {
-  mockSignup: (user, options) => {
 
+  OPTIONS: {
+    USER: {
+      LOGGED_IN: true,
+
+    }
+
+  }
+
+  mockUser: (user, options) => {
+    const idCollection = [];
+    
+    return idCollection;
   },
-  teardownMockSignup: (id, options) => {
 
+  tearDown: (idCollection) => {
+    const documentDeletions = [];
+    for (const document of idCollection) {
+      const { type, id } = document;
+      let deletionPromise;
+      switch (type) {
+        case MODELS.SESSION:
+          deletionPromise = Session.findByIdAndDelete(id).exec();
+          break;
+        case MODELS.USER:
+          deletionPromise = User.findByIdAndDelete(id).exec();
+          break;
+        case MODELS.DECK:
+          deletionPromise = Deck.findByIdAndDelete(id).exec();
+          break;
+        case MODELS.CARD:
+          deletionPromise = Flashcard.findByIdAndDelete(id).exec();
+          break;
+        default:
+          break;
+      }
+      if (deletionPromise) documentDeletions.push(deletionPromise);
+    }
+    return Promise.all(documentDeletions).catch(() => {
+      console.log("Warning: teardown failed");
+    });
   },
 };
